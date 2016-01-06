d31e72a292f1986b685530d2d74eb752e697ae9a
Péter Kalicz <kaliczp@gmail.com>
2016-01-06 12:08:55 +0100

    Histogram enhancement

--- Histogrammhoz.R
+++ Histogrammhoz.R
@@ -60,6 +60,42 @@ plot.density.timewindow <- function(x, time.window=c('2010/2040','2040/2070','20
 }
 
 plot.density.timewindow(apply.yearly (et.pred.remo$ET_M, max),main="",xlab="")
-plot.density.timewindow(apply.yearly (et.pred.remo$SOIL_M, min), szorzo=1.2, legend.place="right", main="", xlab="SM min [mm]", ylab="Rel. gyak.")
+
+plot.density.timewindow(apply.yearly (et.pred.remo$SOIL_M, mean), szorzo=1.3, legend.place="right", main="", xlab="SOIL_M átlag [mm]", ylab="Rel. gyak.")
+plot.density.timewindow(apply.yearly (et.pred.remo$ET_M, mean), szorzo=1.2, main="", ylab="Rel. gyak.", xlab="ET átlag [mm/hó]")
+
+
+plot.density.timewindow(apply.yearly (et.pred.remo$SOIL_M, min), szorzo=1.2, legend.place="right", main="", xlab="SOIL_M min [mm]", ylab="Rel. gyak.")
 plot.density.timewindow(apply.yearly (et.pred.remo$ET_M, max), szorzo=1.2, main="", ylab="Rel. gyak.", xlab="ET max [mm/hó]")
 
+
+pdf(width=12,height=3.5)
+par(mar=c(2.1,4.1,0.6,1.1))
+
+plot.zoo(as.zoo(et.pred.remo['2010/2100',1]),xaxs = "i",ylab="ET_M [mm/hó]",main="",xaxt="n",xlab="", type="n")
+grid(nx=NA,ny=NULL, lwd=2, col="grey")
+lines(as.zoo(et.pred.remo['2010/2100',1]),lwd=2)
+## MIÉRT csúszik el?
+axis.POSIXct(1,at=seq(as.POSIXct("2010-01-31"),as.POSIXct("2100-12-31"),by="10 years"))
+
+plot.zoo(as.zoo(et.pred.remo['2010/2100',2]),xaxs = "i",ylab="SOIL_M [mm]",main="",xaxt="n",xlab="")
+grid(nx=NA,ny=NULL, lwd=2, col="grey")
+lines(as.zoo(et.pred.remo['2010/2100',2]), lwd=2)
+axis.POSIXct(1,at=seq(as.POSIXct("2010-01-31"),as.POSIXct("2100-12-31"),by="10 years"))
+
+## Színes időszakok
+plot.zoo(as.zoo(et.pred.remo['2010/2100',1]),xaxs = "i",ylab="ET_M [mm/hó]",main="",xaxt="n",xlab="",type="n")
+grid(nx=NA,ny=NULL, lwd=2, col="grey")
+lines(as.zoo(et.pred.remo['2010/2040',1]),col=3,lwd=2)
+lines(as.zoo(et.pred.remo['2040/2070',1]),col=2,lwd=2)
+lines(as.zoo(et.pred.remo['2070/2100',1]),col=1,lwd=2)
+axis.POSIXct(1,at=seq(as.POSIXct("2010-01-31"),as.POSIXct("2100-12-31"),by="10 years"))
+legend("topleft",c('2010/2040','2040/2070','2070/2100'),ncol=3,lwd=2,col=3:1)
+
+plot.zoo(as.zoo(et.pred.remo['2010/2100',2]),xaxs = "i",ylab="SOIL_M [mm]",main="",xaxt="n",xlab="", type="n")
+grid(nx=NA,ny=NULL, lwd=2, col="grey")
+lines(as.zoo(et.pred.remo['2010/2040',2]), lwd=2, col=3)
+lines(as.zoo(et.pred.remo['2040/2070',2]), lwd=2, col=2)
+lines(as.zoo(et.pred.remo['2070/2100',2]), lwd=2, col=1)
+axis.POSIXct(1,at=seq(as.POSIXct("2010-01-31"),as.POSIXct("2100-12-31"),by="10 years"))
+legend("bottomleft",c('2010/2040','2040/2070','2070/2100'),ncol=3,lwd=2,col=3:1)
