007055b8b247b54abd7df0fb6e0ad8ab6c983a1e
Péter Kalicz <kaliczp@gmail.com>
2016-01-26 09:27:40 +0100

    Reorganise optimisation test plot
    Firstly a data.frame created with the soil.max values. The et.test script
    put all calculated RSS value into the data.frame RSS column.

--- testettest.R
+++ testettest.R
@@ -3,11 +3,11 @@ et.test(200, test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CRE
 SOIL.MAX <- optimize(et.test, interval=c(100,10000), temp = test.meteo.xts$t, prec=test.meteo.xts$P, pet.real=PET.proj, cremap=test.meteo.xts$ET-CREMAP)$minimum
 
 ## Visual scanning SOIL.MAX parameter
-plot(700,et.test(700,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP),xlim=c(100,10000),ylim=c(1000,300000),type="n", xlab="SOIL_MAX", ylab="RSS")
-for(tti in seq(100,1000,50)){
-  points(tti,et.test(tti,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP), pch=".")
+tempsoilm <- seq(100,1000,50)
+tempsoilm.df <- data.frame(soilmax=tempsoilm, RSS=numeric(length(tempsoilm)))
+for(tti in 1:nrow(tempsoilm.df)){
+  tempsoilm.df[tti,"RSS"] <- et.test(tempsoilm.df[tti,"soilmax"],test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP)
 }
-axis(1,500,tck=1,lab=F)
-SOIL.MAX <- 500
+plot(tempsoilm.df, type="p", , pch=".", xlab="SOIL_MAX", ylab="RSS")
 
 et.calc(SOIL.MAX,Temp = test.meteo.xts$t, Prec=test.meteo.xts$P, PET.real=PET.proj)
