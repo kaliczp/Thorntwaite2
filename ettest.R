f6bf044df4e96b47ef170a77f4ce82b55d9f1668
András Herceg <gundam@freemail.hu>
2016-01-26 09:11:31 +0100

    Improve optimization.

--- ettest.R
+++ ettest.R
@@ -2,5 +2,6 @@ et.test <- function(soil.max, temp, prec, pet.real, cremap) {
     
     et.pred.prelim <- et.calc(SOIL_MAX = soil.max, Temp = temp, Prec= prec, PET.real=pet.real)
     valid.difference <- na.omit(cremap - et.pred.prelim$ET_M)
-    sum(valid.difference^2)
+    val.diff.sum <- sum(valid.difference^2)
+    val.diff.sum +  val.diff.sum/1000 * soil.max^2/1000
 }
--- testettest.R
+++ testettest.R
@@ -1,11 +1,11 @@
-et.test(200, test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts[,1])
+et.test(200, test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP)
 
-SOIL.MAX <- optimize(et.test, interval=c(100,10000), temp = test.meteo.xts$t, prec=test.meteo.xts$P, pet.real=PET.proj, cremap=test.meteo.xts[,1])$minimum
+SOIL.MAX <- optimize(et.test, interval=c(100,10000), temp = test.meteo.xts$t, prec=test.meteo.xts$P, pet.real=PET.proj, cremap=test.meteo.xts$ET-CREMAP)$minimum
 
 ## Visual scanning SOIL.MAX parameter
-plot(700,et.test(700,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts[,1]),xlim=c(100,10000),ylim=c(1000,300000),type="n", xlab="SOIL_MAX", ylab="RSS")
-for(tti in 100:10000){
-  points(tti,et.test(tti,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts[,1]), pch=".")
+plot(700,et.test(700,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP),xlim=c(100,10000),ylim=c(1000,300000),type="n", xlab="SOIL_MAX", ylab="RSS")
+for(tti in seq(100,1000,50)){
+  points(tti,et.test(tti,test.meteo.xts$t, test.meteo.xts$P, PET.proj, test.meteo.xts$ET-CREMAP), pch=".")
 }
 axis(1,500,tck=1,lab=F)
 SOIL.MAX <- 500
