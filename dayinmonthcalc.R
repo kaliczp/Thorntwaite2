59681064cf0f38de9841aac09843f324a8967f51
Péter Kalicz <kaliczp@gmail.com>
2018-03-20 17:28:32 +0100

    Earlier implicit difftime calibration now explicit in dayinmonthcalc
    The PETH.gen throw an error because the errorneous multiplication
    with difftime object. Earlier an implicit conversion is used to
    change difftime to simple numeric. Now it is converted to explicit.

--- dayinmonthcalc.R
+++ dayinmonthcalc.R
@@ -1,6 +1,8 @@
 day.in.month.calc <- function(x) {
     ## The times necessary to endin in 31 days long month
     differences.in.days <- diff(x)
-    ## Add the last value and impicitly convert to numeric vector
+    ## Convert to numeric vector
+    differences.in.days <- as.numeric(differences.in.days)
+    ## Add the last value the last december
     c(differences.in.days, 31)
 }
